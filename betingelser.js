aaadc6ea8c8546590ad8b1b8303e1494998d2c4f
millehelbo <5123658@rts-365.dk>
2018-01-19 13:24:11 +0100

    betingelser

---
+++ betingelser.js
@@ -0,0 +1,25 @@
+// let boolean = false; 
+
+// if(boolean == true){
+//     "Hej verden";
+//     console.log("Hej verden");
+// };
+
+// let tal = 8;
+// // console.log(tal);
+
+// if(tal > 5){
+//     console.log("højere end 5");
+// }else{
+//     console.log("lavere end 5");
+// }
+
+// let tal = 8;
+// console.log((tal < 3 ? "mindre end 3" : "højere end 3"));
+
+// let vitaminwell = "Antioxidant"
+// if(vitaminwell == "Antioxidant"){
+//     console.log("Vitamin Welll Antioxidant indholder C+E vitamin.");
+// }else{
+//     console.log("Nej");
+// }
