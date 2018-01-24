9066511357d83cce4d9c24a12fe3a78f96b7a0da
millehelbo <5123658@rts-365.dk>
2018-01-24 10:11:44 +0100

    funktioner

---
+++ funktioner.js
@@ -0,0 +1,36 @@
+// function gange(x, y){
+     
+//         return x * y;
+//     }
+
+// console.log(gange(7.333, 3.245));
+
+// function gange(x, y){
+//     if (isNaN(x)){
+//         return "Ikke tal";
+//     }else{
+//         return x * y;
+//     }
+// }
+
+// console.log(gange("haj", "nemo"));
+
+// function gange(x, y){
+//     if (isNaN(x) || isNaN(y)){
+//         return "Ikke tal";
+//     }else{
+//         return x * y;
+//     }
+// }
+
+// console.log(gange("3.687", "9.284"));  
+
+// function gange(x){
+//     if (isNaN(x)){
+//         return "Ikke tal";
+//     }else{
+//         return x;
+//     }
+// }
+
+// console.log(gange(3.687));  
