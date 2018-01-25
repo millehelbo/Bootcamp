4273b8b7b409fe58ad48ae23210f1f0adeafd75c
millehelbo <5123658@rts-365.dk>
2018-01-25 14:34:54 +0100

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,30 @@
+
+// let tal = [1, 2, 3, 4, 5, 6];
+// tal.forEach(test);
+
+// function test(tal){
+//     console.log("<p>" + tal + "<p>");
+// }
+
+// function haj(data, callback){
+//     callback(data);
+// };
+
+// function fisk(data){
+//     console.log("<p>" + data + "</p>")
+// };
+
+// haj("nemo", fisk);
+
+
+
+// function iceTea(data, callback){
+//         callback(data);
+// };
+
+// function haj(data){
+//         console.log("haj" + data + "fisk")
+// };
+
+// iceTea("nemo", haj);
+
